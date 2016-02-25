844045f5a66ff0e2ae3ed498388efaf8e0e56be4
Prabhsharan Singh Brar <prabh182006@gmail.com>
2016-02-25 22:53:53 +0000

    updated migration for the users table

--- db/migrate/20160225014845_create_tables.rb
+++ db/migrate/20160225014845_create_tables.rb
@@ -3,7 +3,6 @@ class CreateTables < ActiveRecord::Migration
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.string :username
       t.string :email
       t.string :password_digest
       t.timestamps null: false
