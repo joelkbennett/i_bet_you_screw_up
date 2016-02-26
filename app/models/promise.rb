d3c564e869db67ecded7164bb20a697cd8f602da
Prabhsharan Singh Brar <prabh182006@gmail.com>
2016-02-26 03:24:31 +0000

    corrected the expiration date logic

--- app/models/promise.rb
+++ app/models/promise.rb
@@ -9,7 +9,7 @@ class Promise < ActiveRecord::Base
   validate :expiration_date_cannot_be_in_the_past, if: :expires_at
 
   def hours_until_expired
-    ((expires_at.to_time - DateTime.now - 8.hours)) / 1.hours).ceil
+    ((expires_at.to_time - (DateTime.now - 8.hours)) / 1.hours).ceil
   end
 
   def find_user
