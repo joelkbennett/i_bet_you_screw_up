39b51a98999fe494d0c3298d275cdfb7609e27fa
Prabhsharan Singh Brar <prabh182006@gmail.com>
2016-02-26 17:38:26 +0000

    implemented cards

--- app/models/promise.rb
+++ app/models/promise.rb
@@ -2,7 +2,6 @@ class Promise < ActiveRecord::Base
 
   belongs_to :user
   has_many :bets
-  has_many :comments
 
   validates :content, presence: true
   validates :expires_at, presence:true
@@ -10,11 +9,12 @@ class Promise < ActiveRecord::Base
   validate :expiration_date_cannot_be_in_the_past, if: :expires_at
 
   def hours_until_expired
-    ((expires_at.to_time - (DateTime.now - 8.hours)) / 1.hours).ceil
-  end
-
-  def ordered_comments
-    comments.order(created_at: :desc)
+    hours = ((expires_at.to_time - (DateTime.now - 8.hours)) / 1.hours).ceil
+    if hours > 0
+      "Expires in #{hours} hours!"
+    else
+      "Expired!"
+    end
   end
 
   private
