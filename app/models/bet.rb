c0feaca4ed610163f7e99da453b5758a470d7ced
joelkbennett <joelkbennett@gmail.com>
2016-02-27 00:31:39 +0000

    Update user image on promise; Edit best to apply custom bet value if given

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -16,7 +16,7 @@ class Bet < ActiveRecord::Base
   end
 
   def apply_bet
-    in_favour == promise.validated ? user.add_points(DEFAULT_BET) : user.subtrack_points(DEAFULT_BET)
+    in_favour == promise.validated ? user.add_points(bet_value.to_i || DEFAULT_BET) : user.subtrack_points(bet_value.to_i || DEAFULT_BET)
   end
 
   private
