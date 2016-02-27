c9dbb4918ac6424d416d4b3625e939e59277a7c8
Prabhsharan Singh Brar <prabh182006@gmail.com>
2016-02-27 22:01:03 +0000

    merged conflict

--- app/actions.rb
+++ app/actions.rb
@@ -122,12 +122,8 @@ post '/promises/:id/validate' do |id|
   @promise = Promise.find(id)
   validation = true if params[:yes] == "Promise Kept"
   validation = false if params[:no] == "Promise Not Kept"
-<<<<<<< HEAD
-  @promise.update(validated: validation)
-  binding.pry
-=======
   @promise.update(validated: validation, expires_at: DateTime.now - 8.hours)
->>>>>>> implement-jquery
+  binding.pry
   @promise.apply_promise_value
   @promise.bets.each { |bet| bet.apply_bet }
   redirect "/promises/#{id}"
