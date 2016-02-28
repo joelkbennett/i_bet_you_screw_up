1c193afb735bcf45278554dadfbefb43f25146cb
joelkbennett <joelkbennett@gmail.com>
2016-02-28 19:44:40 +0000

    Fix layout of all comments on a promise

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -92,6 +92,7 @@ $(function() {
         method: 'POST',
         data: { body: commentBody }
     }).done(function(res) {
+        commentForm.find('#comment-body').val('');
         commentsList.prepend(appendComment(res));
     });
   });
@@ -110,4 +111,31 @@ $(function() {
     return commentEl; 
   }
 });
+
+// Fade box
+
+$(document).ready(function() {
+  var element = document.getElementById("js-fadeInElement");
+  $(element).addClass('js-fade-element-hide');
+
+  $(window).scroll(function() {
+    if( $("#js-fadeInElement").length > 0 ) {
+      var elementTopToPageTop = $(element).offset().top;
+      var windowTopToPageTop = $(window).scrollTop();
+      var windowInnerHeight = window.innerHeight;
+      var elementTopToWindowTop = elementTopToPageTop - windowTopToPageTop;
+      var elementTopToWindowBottom = windowInnerHeight - elementTopToWindowTop;
+      var distanceFromBottomToAppear = 300;
+
+      if(elementTopToWindowBottom > distanceFromBottomToAppear) {
+        $(element).addClass('js-fade-element-show');
+      }
+      else if(elementTopToWindowBottom < 0) {
+        $(element).removeClass('js-fade-element-show');
+        $(element).addClass('js-fade-element-hide');
+      }
+    }
+  });
+});
+
 
